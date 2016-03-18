0f1f6985f51c662a01696e2fb5ee1fb2ff40710e
air2637 <air2637@gmail.com>
2016-03-18 19:39:15 +0800

    First Commit

---
+++ Graph.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Graph {
+	List<Node> nodes;
+	List<Edge> edges;
+	
+	public Graph (List<Node> nodes, List<Edge> edges){
+		this.nodes = nodes;
+		this.edges = edges;
+	}
+}
